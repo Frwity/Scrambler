b82231e01ec6a1d705785d6fae444da7178081f3
Antony Gavelle <a.gavelle@student.isartdigital.com>
2020-03-19 14:07:04 +0100

    Base Proto

---
+++ Assets/HoverEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverEffect : MonoBehaviour
+{
+    [SerializeField]
+    private float hoverSpeed;
+    [SerializeField]
+    private float maxHeight;
+    [SerializeField]
+    private float minHeight;
+
+    private float hoverHeight;
+    private float hoverRange;
+
+    void Start()
+    {
+        hoverHeight = (maxHeight + minHeight) / 2.0f;
+        hoverRange = maxHeight - minHeight;
+    }
+
+    void Update()
+    {
+        transform.Translate(0, (hoverHeight + Mathf.Cos(Time.time * hoverSpeed) * hoverRange) * Time.deltaTime, 0);
+    }
+}
