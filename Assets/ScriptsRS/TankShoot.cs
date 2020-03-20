1d3cec0fe20d54917fd77484820e8497d5dd7f34
r.sandrin <r.sandrin@student.isartdigital.com>
2020-03-20 16:26:49 +0100

    added some parameters

--- Assets/ScriptsRS/TankShoot.cs
+++ Assets/ScriptsRS/TankShoot.cs
@@ -1,20 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TankShoot : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private Vector2 shootingDir;
+    
+    [SerializeField] private int angleInDeg = 0;
+    [SerializeField] private int precision = 0;
     [SerializeField] private float shootingStrength;
+    [Header("Prefab")]
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float cooldown = 0;
-    private float currentCooldown ;
+    [SerializeField] private int numberOfProjectile = 1;
     
+    private float currentCooldown ;
+    private Vector2 shootingDir;
+    private int lastAngle;
+    private int lastRotationAngle;
+    Vector2 Rotate(Vector2 aPoint, float aDegree)
+    {
+        return Quaternion.Euler(0,0,aDegree) * aPoint;
+    }
     void Start()
     {
-        
-        shootingDir.Normalize();
+        shootingDir = Rotate(transform.right, angleInDeg);
+        lastAngle = angleInDeg;
         currentCooldown = 0;
         
     }
@@ -22,12 +35,26 @@ public class TankShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (lastAngle != angleInDeg || lastRotationAngle != (int)transform.rotation.y)
+        {
+            Debug.Log(transform.right);
+            shootingDir = Rotate(transform.right,  angleInDeg);
+            lastAngle = angleInDeg;
+            lastRotationAngle = (int)transform.rotation.y;
+        }
+        
         if (Input.GetKey(KeyCode.F) && currentCooldown <= 0.01f)
         {
-            GameObject bulletInst = Instantiate(bulletPrefab, transform.position, transform.rotation);
-            Physics.IgnoreCollision(bulletInst.GetComponent<Collider>(), this.GetComponent<Collider>(), true);
-            Rigidbody rbInst = bulletInst.GetComponent<Rigidbody>();
-            rbInst.AddForce(new Vector3(shootingDir.x, shootingDir.y, 0) * shootingStrength);
+            for (int i = 1; i <= numberOfProjectile; i++)
+            {
+                int angleToAdd = Random.Range(-precision, precision);
+                Vector2 newShootDir = Rotate(shootingDir, angleToAdd);
+                GameObject bulletInst = Instantiate(bulletPrefab, transform.position, transform.rotation);
+                Physics.IgnoreCollision(bulletInst.GetComponent<Collider>(), this.GetComponent<Collider>(), true);
+                Rigidbody rbInst = bulletInst.GetComponent<Rigidbody>();
+                rbInst.AddForce(new Vector3(newShootDir.x, newShootDir.y, 0) * shootingStrength);
+            }
+            
             currentCooldown = cooldown;
         }
 
