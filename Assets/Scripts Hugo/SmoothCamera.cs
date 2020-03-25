a9bea2a2138acf7917d1e83c3690ad04d26b65ea
Hugo C <h.chemouny@student.isartdigital.com>
2020-03-25 16:51:08 +0100

    Added a smooth camera with a y inclination (Not fully completed yet.)

---
+++ Assets/Scripts Hugo/SmoothCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 defaultDistance = new Vector3(0, 2f, -10f);
+    [SerializeField] float dampDistance = 10f;
+    [SerializeField] float dampRotation = 10f;
+
+    Transform myTrsf;
+
+    private void Awake()
+    {
+        myTrsf = transform;
+    }
+
+    public void ActualizeTarget()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 ToPos = target.position + (target.rotation * defaultDistance);
+        myTrsf.position = Vector3.Lerp(myTrsf.position, ToPos, dampDistance * Time.deltaTime);
+
+        Quaternion toRot = Quaternion.LookRotation(target.position - myTrsf.position, target.up);
+        myTrsf.rotation = Quaternion.Slerp(myTrsf.rotation, toRot, dampRotation * Time.deltaTime);
+    }
+}
