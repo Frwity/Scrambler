272401d94f5417626b5ddcbbfc7f1158b3c3eae7
Antony Gavelle <a.gavelle@student.isartdigital.com>
2020-04-03 17:44:48 +0200

    added basic explosive barrel

--- Assets/Antony Script/BounceShield.cs
+++ Assets/Antony Script/BounceShield.cs
@@ -21,7 +21,6 @@ public class BounceShield : MonoBehaviour
         {
             Vector3 direction = collision.gameObject.GetComponent<BulletSharedClass>().direction;
             collision.gameObject.transform.Rotate(transform.forward, Vector3.SignedAngle(direction, transform.right, Vector3.forward) * 2 + 180);
-            //collision.gameObject.GetComponent<BulletSharedClass>().direction = new Vector3(-direction.x, -direction.y, 0);
         }
     } 
 }
---
+++ Assets/Antony Script/ExplosiveBarrel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveBarrel : MonoBehaviour
+{
+    [SerializeField] private float explosionRange;
+    [SerializeField] private int damage;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void Explode()
+    {
+        int visionMask = 1 << 13;
+        int hoverMask = 1 << 8;
+        int bulletMask = 1 << 10;
+        int PossessMask = 1 << 15;
+        bulletMask = ~bulletMask;
+        hoverMask = ~hoverMask;
+        visionMask = ~visionMask;
+        PossessMask = ~PossessMask;
+
+        visionMask += hoverMask;
+        visionMask += bulletMask;
+        visionMask += PossessMask;
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRange);
+
+        foreach (Collider inRange in colliders)
+        {
+            if (inRange.CompareTag("Player") || inRange.CompareTag("Enemy"))
+            {
+                Physics.Raycast(transform.position, inRange.transform.position, out RaycastHit hitInfo, explosionRange, visionMask);
+                //Debug.Log(hitInfo.collider.gameObject.tag);
+                if (hitInfo.collider.gameObject.CompareTag("Player") || hitInfo.collider.gameObject.CompareTag("Enemy"))
+                    inRange.GetComponent<Entity>().InflictDamage(damage);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Explode();
+        }
+    }
+}
