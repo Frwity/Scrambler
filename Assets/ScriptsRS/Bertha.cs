1d382123fd94456364c4f531dbc3b3b740bfd1da
r.sandrin <r.sandrin@student.isartdigital.com>
2020-04-09 13:31:59 +0200

    Added Bertha functionality with bullets

--- Assets/ScriptsRS/Bertha.cs
+++ Assets/ScriptsRS/Bertha.cs
@@ -9,12 +9,18 @@ public class Bertha : MonoBehaviour
     [SerializeField] private float shootForce = 0.0f;
     [SerializeField] private float damage;
     [SerializeField] private Entity Drone;
+    private DroneSkill ds;
     [SerializeField] private GameObject bullet;
     private float currCooldown = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponentInChildren<Teleport>().toTP = Drone.transform;
+        if (Drone != null)
+        {
+            ds = Drone.GetComponent<DroneSkill>();
+            ds.OnShoot.AddListener(shootBullet);
+        }
     }
 
     // Update is called once per frame
@@ -34,9 +40,13 @@ public class Bertha : MonoBehaviour
 
     void shootBullet()
     {
+        if (currCooldown < Cooldown)
+        {
+            
+            return;
+        }
         GameObject bu = Instantiate(bullet, transform.GetChild(0).position, transform.rotation);
-        BerthaBullet bbu = bu.GetComponent<BerthaBullet>();
-        bbu.toTP = Drone.transform;
+        bu.transform.localScale += new Vector3(1,1,1);
         Physics.IgnoreCollision(bu.GetComponent<Collider>(), Drone.GetComponent<Collider>(), true);
         Physics.IgnoreCollision(bu.GetComponent<Collider>(), GetComponent<Collider>(), true);
         Rigidbody rb = bu.GetComponent<Rigidbody>();
