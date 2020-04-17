a7c68f149757b58d452ee79e1fef27d282588212
r.sandrin <r.sandrin@student.isartdigital.com>
2020-04-17 15:52:57 +0200

    modified the way the bertha spawn the drone

---
+++ Assets/ScriptsRS/SpawnPointData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]public class SpawnPointData
+{
+        
+    [HideInInspector]public Transform ParentPos = new RectTransform() ;
+
+    [SerializeField]private Vector3 pos;
+    public float radius;
+
+
+    [HideInInspector]
+    public Vector3 SpawnPointPos
+    {
+        get { return pos + ParentPos.position; }
+        set { pos = value - ParentPos.position; }
+    }
+
+    public Color color = Color.black;
+    public bool enabled;
+
+}
