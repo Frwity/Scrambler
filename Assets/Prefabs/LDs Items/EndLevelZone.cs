9a12422c8b146a7c4b177d22afbea67efcc98b79
Hugo C <h.chemouny@student.isartdigital.com>
2020-04-21 16:34:46 +0200

    Zone to end a level created

---
+++ Assets/Prefabs/LDs Items/EndLevelZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelZone : MonoBehaviour
+{
+    [SerializeField] Color gizmoColor = Color.blue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        BoxCollider box = GetComponent<BoxCollider>();
+        Gizmos.color = gizmoColor;
+
+        Gizmos.DrawWireCube(box.center, box.size);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject.FindGameObjectWithTag("EndScreen").GetComponent<Animator>().SetTrigger("Win");
+
+            other.GetComponentInParent<PlayerControl>().enabled = false;
+        }
+    }
+}
