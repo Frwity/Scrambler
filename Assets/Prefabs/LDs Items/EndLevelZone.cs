5486873cdf4580a8a520d2394a9c265a98db14e0
Hugo C <h.chemouny@student.isartdigital.com>
2020-04-21 16:39:57 +0200

    Quick fix !
    The flash should be pre-fixed with the new model of the virus.

--- Assets/Prefabs/LDs Items/EndLevelZone.cs
+++ Assets/Prefabs/LDs Items/EndLevelZone.cs
@@ -6,12 +6,6 @@ public class EndLevelZone : MonoBehaviour
 {
     [SerializeField] Color gizmoColor = Color.blue;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     private void OnDrawGizmos()
     {
         BoxCollider box = GetComponent<BoxCollider>();
